7f15f8d9785e6cc431965e8fc65591559bd5a63f
dharmveer47 <singhdharmu89@gmail.com>
2022-05-11 10:37:42 +0530

    initial

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useState } from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import Header from "./components/Header";
+const initial = [
+  { text: "buy coffee", key: "1" },
+  { text: "create an app", key: "2" },
+  { text: "Play on swithc", key: "3" },
+];
 export default function App() {
+  const [todo, setTodo] = useState(initial);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <View style={styles.content}>
+        {/*  To form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todo}
+            renderItem={({ item }) => <Text>{item.text}</Text>}
+          />
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
   },
 });
---
+++ components/Header.js
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <View style={styles.header}>
+      <Text styles={styles.title}>Header</Text>
+    </View>
+  )
+}
+
+export default Header
+
+const styles = StyleSheet.create({
+    header: {
+        height: 80,
+        paddingTop: 38,
+        backgroundColor: 'coral'
+    },
+    title: {
+        textAlign: 'center',
+        color: '#fff',
+        fontWeight: 'bold',
+    }
+})
